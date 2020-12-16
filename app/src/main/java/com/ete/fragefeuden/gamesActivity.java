7e7d156bea11a6be5e4913b1ddd286cab5102f16
John <engblomsandinjohn@gmail.com>
2020-12-16 13:57:53 +0100

    GameActivity är broken men dynamic links behövs

--- app/src/main/java/com/ete/fragefeuden/gamesActivity.java
+++ app/src/main/java/com/ete/fragefeuden/gamesActivity.java
@@ -2,13 +2,49 @@ package com.ete.fragefeuden;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class gamesActivity extends AppCompatActivity {
+
+    private List<Game> gameList = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
     }
+
+
+    //Denna kallas när man startar nytt game
+    protected void createNewGame(){
+
+    }
+
+    //Denna kallas när man fortsätter ett spel som redan är igång
+    protected void continueGame(){
+
+    }
+
+    public static Uri generateContentLink() {
+        Uri baseUrl = Uri.parse("https://your-custom-name.page.link");
+        String domain = "https://your-app.page.link";
+
+        DynamicLink link = FirebaseDynamicLinks.getInstance()
+                .createDynamicLink()
+                .setLink(baseUrl)
+                .setDomainUriPrefix(domain)
+                .setIosParameters(new DynamicLink.IosParameters.Builder("com.your.bundleid").build())
+                .setAndroidParameters(new DynamicLink.AndroidParameters.Builder("com.your.packageName").build())
+                .buildDynamicLink();
+
+        return link.getUri();
+    }
+
+
 }
