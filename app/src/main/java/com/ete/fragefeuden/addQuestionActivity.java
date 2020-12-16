564ff7703b7a22b84403bf66c6a2b7a261f41327
William <williamtiderman@live.com>
2020-12-16 13:55:38 +0100

    Försök till att lägga til frågor //William

--- app/src/main/java/com/ete/fragefeuden/addQuestionActivity.java
+++ app/src/main/java/com/ete/fragefeuden/addQuestionActivity.java
@@ -5,30 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class addQuestionActivity extends AppCompatActivity {
+
+    DatabaseReference myRef;
+    EditText question, correct, wrong1, wrong2, wrong3;
+    databaseAddQuestion databaseAddQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_question);
+        question = findViewById(R.id.editTextQuestion);
+        correct = findViewById(R.id.editTextCorrect);
+        wrong1 = findViewById(R.id.editTextWrong1);
+        wrong2 = findViewById(R.id.editTextWrong2);
+        wrong3 = findViewById(R.id.editTextWrong3);
+        databaseAddQuestion = new databaseAddQuestion();
+        myRef = FirebaseDatabase.getInstance().getReference().child("dataBaseAddActivity");
     }
 
     public void onSendQuestion(View view) {
-        EditText question = (EditText) findViewById(R.id.editTextQuestion);
-        EditText correct = (EditText) findViewById(R.id.editTextCorrect);
-        EditText wrong1 = (EditText) findViewById(R.id.editTextWrong1);
-        EditText wrong2 = (EditText) findViewById(R.id.editTextWrong2);
-        EditText wrong3 = (EditText) findViewById(R.id.editTextWrong3);
 
-        String questionString = question.getText().toString();
-        String correctString = correct.getText().toString();
-        String wrong1String = wrong1.getText().toString();
-        String wrong2String = wrong2.getText().toString();
-        String wrong3String = wrong3.getText().toString();
+        databaseAddQuestion.setCorrectString(question.getText().toString());
+        databaseAddQuestion.setCorrectString(correct.getText().toString());
+        databaseAddQuestion.setWrong1String(wrong1.getText().toString());
+        databaseAddQuestion.setWrong2String(wrong2.getText().toString());
+        databaseAddQuestion.setWrong3String(wrong3.getText().toString());
 
+        myRef.push().setValue(databaseAddQuestion);
         //TODO skicka värden till databas
 
         question.setText("");
